9c25aca03e896914a9084404ed3b7a4037a94a3a
Chris Bukuba <cbukuba@fu-r3-p4.s19.be>
2021-10-02 13:08:19 +0200

    isalnum_v.1+fix

--- libc/functions/ft_isalnum.c
+++ libc/functions/ft_isalnum.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/02 11:14:41 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/10/02 13:06:36 by cbukuba          ###   ########.fr       */
+/*   Created: 2021/10/02 12:48:57 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/10/02 13:07:06 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,27 @@ int	ft_isalpha(int c)
 		return (0);
 }
 
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_isalnum(int c)
+{
+	if (ft_isalpha(c) == 1 || ft_isdigit(c) == 1)
+		return (1);
+	else
+		return (0);
+}
+
 // #include <stdio.h>
 // int main()
 // {
 //     char c = '&';
-//     if (ft_isalpha(c) == 1)
+//     if (ft_isalnum(c) == 1)
 //         printf("Carre");
 //     else
 //         printf("hanhan");
