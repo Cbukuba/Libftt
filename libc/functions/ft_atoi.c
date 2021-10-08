8ebb852aadafa0454829fb3c0fdac5d23520b752
Chris Bukuba <cbukuba@fu-r5-p4.s19.be>
2021-10-08 15:05:29 +0200

    atoi_v.1

--- libc/functions/ft_atoi.c
+++ libc/functions/ft_atoi.c
@@ -6,11 +6,10 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 17:16:46 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/10/07 18:49:02 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/10/08 15:03:58 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Mettre commentaire en anglais
 #include <stdlib.h>
 
 int	ft_atoi(const char *str)
@@ -24,25 +23,23 @@ int	ft_atoi(const char *str)
 	sign = 1;
 	ent = 0;
 	if (str[0] == '-')
+	{
 		sign *= -1 ;
-		i++;
+		i ++;
+	}	
 	while (str[i] >= 48 && str[i] <= 57)
 	{
 		new_ent = ent * 10 + (str[i] - 48);
-		// condition pour les overflows
-		// car quand on atteint int_max machine fait +1
-		if (new_ent < ent)
-			return (0);
 		i ++;
 		ent = new_ent;
 	}
 	return (ent * sign);
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-	printf("%d\n", atoi("6147483648" ));
-	printf("%d", ft_atoi("6147483648" ));
-}
+// int main()
+// {
+// 	printf("%d\n", atoi("2147483648567" ));
+// 	printf("%d", ft_atoi("2147483648567" ));
+// }
