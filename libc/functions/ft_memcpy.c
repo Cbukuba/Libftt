884c5083b89b13140a3feadf6997451d4bdc53b4
Cbukuba <chrisbukuba058@gmail.com>
2021-10-14 20:23:42 +0200

    memcpy_v.1.2

--- libc/functions/ft_memcpy.c
+++ libc/functions/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 09:17:51 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/10/12 09:56:57 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/10/14 20:22:19 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,30 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	int		i;
+	size_t		i;
 	char	*new_dst;
 	char	*new_src;
 
 	new_dst = (char *)dst;
 	new_src = (char *)src;
 	i = 0;
-	while (i < n)
+	while (new_src[i] && i < n)
 	{
 		new_dst[i] = new_src[i];
 		i ++;
 	}
+	new_dst[i] = '\0';
 	return (dst);
 }
 
-// #include <string.h>
-// #include <stdio.h>
-// int main()
-// {
-//   char s1[] = "Hello";
-//   char s2[] = "Hello";
-//   memcpy(s1,s2,3);
-//   printf("%s\n", s1);
-//   char s3[]= "Hello";
-//   char s4[]= "Hello";
-//   ft_memcpy(s3,s4,3);
-//   printf("%s", s3);
-// }
+#include <string.h>
+#include <stdio.h>
+int main()
+{
+  char s1[]= "Hello--world";
+  memcpy(s1 + 6,s1 + 7,7);
+  printf("%s\n", s1);
+  char s3[]= "Hello--world";
+  ft_memcpy(s3 + 6,s3 + 7,7);
+  printf("%s", s3);
+}
