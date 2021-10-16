e5bca48959612c76a19e52ea82b647588a29477c
Cbukuba <chrisbukuba058@gmail.com>
2021-10-16 23:13:38 +0200

    memmove_v.1.2

--- libc/functions/ft_memmove.c
+++ libc/functions/ft_memmove.c
@@ -6,43 +6,43 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 10:22:29 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/10/12 11:33:48 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/10/16 23:11:59 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // manqu les conditions basiques (quand il n y a pas de overlap)
 #include <stdlib.h>
+#include <string.h>
 
 void *ft_memmove(void *dst, const void *src, size_t len)
 {
-	int		i;
+	size_t	i;
 	int		y;
 	char	*new_dst;
 	char	*new_src;
-	char	buffer_temp[2];
+	char	buffer_temp[3];
 
 	new_dst = (char *)dst;
 	new_src = (char *)src;
 	i = 0;
 	if (new_src > new_dst)
-	{
-		new_src[i] = buffer_temp[i];
-		buffer_temp[i] = new_dst[i];
+	{	
+		new_dst[i] = buffer_temp[i];
+		buffer_temp[i] = new_src[i];
 		i ++;
 	}
-	if (new_src <= new_src)
-		return memcpy(dst, src, len);
-
+	memcpy(dst,src,len);
 	return dst;
 }
 
-#include <string.h>
 #include <stdio.h>
 int main()
 {
-  char s1[20] = "Hello my dear";
-  char s2[50] = "wassup youtube maadou gangseuleu";
-  char *p = ft_memmove (s1 + 4,s2, 16);
-  char *u = memmove (s1 + 4,s2, 16);
-  printf("%s\n%s", u, p);
+	char s1[20] = "Hello my dear";
+	char s2[50] = "wassup youtube maadou gangseuleu";
+	char s3[20] = "Hello my dear";
+	char s4[50] = "wassup youtube maadou gangseuleu";
+	ft_memmove(s3, s4, 15);
+	memmove(s1, s2, 15);
+	printf("%s\n%s", s1, s3);
 }
