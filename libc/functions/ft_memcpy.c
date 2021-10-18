a41d729d88764a7acb296856314f67e2d35311e9
Chris Bukuba <cbukuba@fu-r5-p4.s19.be>
2021-10-18 08:45:08 +0200

    memcopy & memmove_v.1.3

--- libc/functions/ft_memcpy.c
+++ libc/functions/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 09:17:51 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/10/14 20:22:19 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/10/18 08:41:29 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t		i;
+	size_t	i;
 	char	*new_dst;
 	char	*new_src;
 
@@ -30,14 +30,14 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	return (dst);
 }
 
-#include <string.h>
-#include <stdio.h>
-int main()
-{
-  char s1[]= "Hello--world";
-  memcpy(s1 + 6,s1 + 7,7);
-  printf("%s\n", s1);
-  char s3[]= "Hello--world";
-  ft_memcpy(s3 + 6,s3 + 7,7);
-  printf("%s", s3);
-}
+// #include <string.h>
+// #include <stdio.h>
+// int main()
+// {
+//   char s1[]= "Hello--world";
+//   memcpy(s1 + 6,s1 + 7,7);
+//   printf("%s\n", s1);
+//   char s3[]= "Hello--world";
+//   ft_memcpy(s3 + 6,s3 + 7,7);
+//   printf("%s", s3);
+// }
--- libc/functions/ft_memmove.c
+++ libc/functions/ft_memmove.c
@@ -6,43 +6,40 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 10:22:29 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/10/16 23:11:59 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/10/18 08:43:58 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// manqu les conditions basiques (quand il n y a pas de overlap)
-#include <stdlib.h>
+//libft.h "ft_memcpy"
 #include <string.h>
 
-void *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
 	int		y;
 	char	*new_dst;
 	char	*new_src;
-	char	buffer_temp[3];
+	char	*buffer_temp;
 
 	new_dst = (char *)dst;
 	new_src = (char *)src;
-	i = 0;
 	if (new_src > new_dst)
 	{	
-		new_dst[i] = buffer_temp[i];
-		buffer_temp[i] = new_src[i];
-		i ++;
+		*new_dst = *buffer_temp;
+		*buffer_temp = *new_src;
 	}
-	memcpy(dst,src,len);
-	return dst;
+	memcpy (dst, src, len);
+	return (dst);
 }
 
-#include <stdio.h>
-int main()
-{
-	char s1[20] = "Hello my dear";
-	char s2[50] = "wassup youtube maadou gangseuleu";
-	char s3[20] = "Hello my dear";
-	char s4[50] = "wassup youtube maadou gangseuleu";
-	ft_memmove(s3, s4, 15);
-	memmove(s1, s2, 15);
-	printf("%s\n%s", s1, s3);
-}
+// #include <stdio.h>
+// int main()
+// {
+// 	char s1[200] = "Hello my dear";
+// 	char s2[7] = "wassup";
+// 	char s3[20] = "Hello my dear";
+// 	char s4[7] = "wassup";
+// 	ft_memmove(s3 + 5, s4, 15);
+// 	memmove(s1 + 5, s2, 15);
+// 	printf("%s\n%s", s1, s3);
+// }
