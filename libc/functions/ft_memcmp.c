13dc2e809f6762f17981e0b65c82e6528b10a11f
Cbukuba <chrisbukuba058@gmail.com>
2021-10-21 15:49:55 +0200

    memcmp_v.1

---
+++ libc/functions/ft_memcmp.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 15:39:16 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/10/21 15:49:26 by cbukuba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	int			i;
+	const char	*str1;
+	const char	*str2;
+
+	i = 0;
+	str1 = (const char *)s1;
+	str2 = (const char *)s2;
+	while (str1[i] != '\0' && str2[i] != '\0' && (str1[i] == str2[i]) && i < n - 1 )
+		i++;
+	if (n <= 0)
+		return (0);
+	return (str1[i] - str2[i]);
+}
+
+// #include <string.h>
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("1: ft_memcmp:  %d\n", ft_memcmp("aaa", "aaa", 20));
+//     printf("      memcmp:  %d\n", memcmp("aaa", "aaa", 20));
+//     printf("2: ft_memcmp:  %d\n", ft_memcmp("aaa", "aaA", 20));
+//     printf("      memcmp:  %d\n", memcmp("aaa", "aaA", 20));
+//     printf("3: ft_memcmp: %d\n", ft_memcmp("aaaaaaaa", "aab", 20));
+//     printf("      memcmp: %d\n", memcmp("aaaaaaaa", "aab", 20));
+//     printf("4: ft_memcmp: %d\n", ft_memcmp("aaa", "aaabcd", 3));
+//     printf("      memcmp: %d\n", memcmp("aaa", "aaabcd", 3));
+//     printf("5: ft_memcmp: %d\n", ft_memcmp("aa0", "aab", 20));
+//     printf("      memcmp: %d\n", memcmp("aa0", "aab", 20));
+//     printf("6: ft_memcmp: %d\n", ft_memcmp("aa", "aab", 0));
+//     printf("      memcmp: %d\n", memcmp("aa", "aab", 0));
+//     printf("7: ft_memcmp: %d\n", ft_memcmp("", "aab", 20));
+//     printf("      memcmp: %d\n", memcmp("", "aab", 20));
+//     printf("8: ft_memcmp:  %d\n", ft_memcmp("", "",-1));
+//     printf("      memcmp:  %d\n", memcmp("", "", -1));
+//     return (0);
+// }
