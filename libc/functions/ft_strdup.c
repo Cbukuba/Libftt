df764715b0b8a910984035be5404c5b080cab8c4
Cbukuba <chrisbukuba058@gmail.com>
2021-10-26 16:29:36 +0200

    strdup_v.1

---
+++ libc/functions/ft_strdup.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/26 15:51:02 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/10/26 16:28:42 by cbukuba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stddef.h>
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	int		lng;
+	char	*p;
+
+	lng = 0;
+	while (s[lng] != '\0')
+		lng ++;
+	p = (char *)malloc(sizeof(char) * lng + 1);
+	if (!p)
+		return (NULL);
+	i = 0;
+	while (i < lng)
+	{
+		p[i] = s[i];
+		i ++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+// #include <stdio.h>
+// #include <string.h>
+// int main()
+// {
+// 	const char s[] = "";
+// 	printf("Source :    %s\n", s);
+// 	printf("ft_strdup : %s\n", ft_strdup(s));
+// 	printf("strdup :    %s\n", strdup(s));
+// }
