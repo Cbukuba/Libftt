13efdeba0437f97c36db4f192e317840fc479657
Cbukuba <chrisbukuba058@gmail.com>
2021-10-26 15:50:38 +0200

    calloc_v.1

---
+++ libc/functions/ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/26 15:09:18 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/10/26 15:49:59 by cbukuba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <limits.h>
+
+void *ft_calloc(size_t nmemb, size_t size)
+{
+	void *ptr;
+	ptr = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	else if ((nmemb * size) > INT_MAX)
+		exit (EXIT_FAILURE);
+	ptr = (int *)malloc(nmemb*size);
+	return (ptr);
+}
