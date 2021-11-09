70d1642b4ed622b85e2fd934a2d0f4d3a02b7ff5
Cbukuba <chrisbukuba058@gmail.com>
2021-11-09 01:41:33 +0100

    split_v.0.1

--- libc/functions/ft_split.c
+++ libc/functions/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 14:51:21 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/11/03 18:31:47 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/11/09 01:40:27 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,107 @@
 #include <stddef.h>
 #include <stdio.h>
 
+size_t	ft_strlen(const char *str)
+{
+	int	c;
+
+	c = 0;
+	while (str[c] != '\0')
+		c ++;
+	return (c);
+}
+
+char	*ft_strcpy(char *dest, char const *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src [i];
+		i ++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = malloc(sizeof(char) * len);
+	if (!str)
+		return (NULL);
+	while (i <= len)
+	{
+		str[i] = s[start + i];
+		i ++;
+	}
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*str;
+	char	*p;
+
+	str = (char *)s;
+	i = 0;
+	while (str[i] != c && str[i])
+		i++;
+	if (str[i] == c)
+		return (str + i);
+	return (NULL);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	int		i;
 	int		j;
 	int		k;
+	int		l;
 	char	**tab;
 
 	i = 0;
 	j = 0;
-	while (*s)
+	while (s[i] != '\0')
 	{		
 		if (s[i] == c)
 			j ++;
 		i ++;
 	}
 	j ++;
-	printf("%d", j);
-	tab = malloc(sizeof(char) * j);
+	i = 0;
+	k = 0;
+	tab = malloc(sizeof(char *) * j);
 	if (!tab)
 		return (NULL);
-	i = 0;
-	j = 1;
-	k = 0;
-	while (s[i])
+	while (i ++ <= j)
 	{
-		while (s[i] != c)
+		tab[i] = malloc(sizeof(char) * (ft_strlen(s)));
+		l = 0;
+		while (s[k] != c && *s)
 		{
-			tab[j][k] = s[i];
-			i ++;
 			k ++;
+			l ++;
 		}
-		i ++;
-		j ++;
+		// printf("k = %d\n\tl = %d\n", k, l);
+		ft_strcpy(tab[i], ft_substr(s, k, l));
+		k ++;
 	}
-	tab[j][i] = '\0';
 	free (tab);
 	return (tab);
- }
-
-
+}
 
 int main()
 {
 	char const	*str = "Wassup-Youtube-Maadou-Gangseuleu";
 	char	del = '-';
-	// char	**tab = 
-	ft_split(str, del);
-	// printf("%s\n", tab[1]);
+	char **tab = ft_split(str, del);
+	int i = 1;
+	while (i ++ <= 4)
+		printf("%s\n", tab[i]);
 }
