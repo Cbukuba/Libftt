ab9027e599c04b7e1011f382e38019d01aacf302
Cbukuba <cbukuba@student.s19.be>
2021-11-15 09:51:55 +0100

    itoa_v.1

--- libc/functions/ft_itoa.c
+++ libc/functions/ft_itoa.c
@@ -6,69 +6,103 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 00:03:37 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/11/13 22:34:34 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/11/15 09:50:49 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stddef.h>
 
-char	*rev_string(int size, char *str)
+size_t	ft_strlen(const char *str)
 {
-	int	i;
-	int	y;
 	int	c;
 
 	c = 0;
-	while (c ++ < size / 2)
+	while (str[c] != '\0')
 	{
-		i = 0;
-		while (i < size / 2)
-		{
-			y = str[i];
-			str[i] = str[size - 1];
-			str[size - 1] = y;
-			i ++;
-			size --;
-		}
+		c ++;
+	}
+	return (c);
+}
+
+char	*rev_string(char *str)
+{
+	int	i;
+	int	y;
+	int	size;
+
+	size = ft_strlen(str) - 1;
+	i = 0;
+	while (i < size)
+	{
+		y = str[i];
+		str[i] = str[size];
+		str[size] = y;
+		i ++;
+		size --;
 	}
 	return (str);
 }
 
-char	*ft_itoa(int n)
+int	intlen(int n)
 {
-	char	*str;
-	int		i;
-	int		res;
-	int		j;
+	int	i;
+
+	i = 0;
+	if (n < 0)
+		i ++;
+	while (n != 0)
+	{
+		n /= 10;
+		i ++;
+	}
+	return (i);
+}
+
+char	*convert(int n, char *str)
+{
+	int	res;
+	int	i;
+	int	j;
 
 	i = 0;
 	j = 0;
-	str = malloc(sizeof(char) * 8);
 	if (n < 0)
 	{
-		n = - n;
+		n = -n;
 		j = 1;
 	}
 	while (n != 0)
 	{
 		res = n % 10;
 		if (res >= 10)
-			str[i] = (res - 10) + 'a';
-		else
-			str[i] = res + '0';
-		n = n / 10;
-		i ++;
+			str[i++] = (res - 10) + 'a';
+		str[i++] = res + '0';
+		n /= 10;
 	}
-	if (j > 0)
-		str[i] = '-';
-	rev_string(i, str);
-	str[i] = '\0';
+	if (j == 1)
+		str[i++] = '-';
 	return (str);
 }
 
-#include <stdio.h>
-
-int	main()
+char	*ft_itoa(int n)
 {
-	printf("%s\n", ft_itoa(-34245));
+	char	*str;
+	int		len;
+
+	len = intlen(n);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	convert(n, str);
+	str[len + 1] = '\0';
+	rev_string(str);
+	return (str);
 }
+
+// #include <stdio.h>
+
+// int	main()
+// {
+// 	printf("%s\n", ft_itoa(-34245));
+// }
