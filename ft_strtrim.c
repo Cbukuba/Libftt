2a73d11a5ad1d7d8aff66358bdbdf110cc6bd3f9
Cbukuba <cbukuba@student.s19.be>
2021-11-17 16:59:11 +0100

    fix_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 11:23:32 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/11/17 16:22:20 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/11/17 16:58:11 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,35 +23,47 @@ size_t	ft_strlen(const char *str)
 	}
 	return (c);
 }
+
+int	find_in_set(char c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i ++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
 	int		i;
 	int		start;
 	int		end;
-	int		str_lng;
-	int		lng;
 
 	start = 0;
-	while (set[start] == s1[start] && s1[start])
+	while (find_in_set(s1[start], set) && s1[start])
 		start ++;
-	end = 0;
-	lng = ft_strlen(s1);
-	while (set[end] == s1[lng - 1])
-		end ++;
-	str_lng = lng - end - start;
-	str = malloc(sizeof(char) * (str_lng + 1));
-	while (start < str_lng)
+	end = ft_strlen(s1);
+	while (find_in_set(s1[end - 1], set) && end > start)
+		end --;
+	str = malloc(sizeof(char) * (end - start + 1));
+	i = 0;
+	while (start < end)
 		str[i ++] = s1[start ++];
+	str[i] = '\0';
 	return (str);
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 int main()
 {
 	char	*str = "   xxxWassup";
 	char	*set = " x";
 
 	printf("original :%s\nStrtrim :%s", str, ft_strtrim(str, set));
-}
-
+}*/
