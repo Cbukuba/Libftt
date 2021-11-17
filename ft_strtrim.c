1e2cd5d67a7a707ad40bc30bce7000d59c7a74bd
Cbukuba <cbukuba@student.s19.be>
2021-11-17 16:22:50 +0100

    vim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,38 +6,52 @@
 /*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 11:23:32 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/11/16 13:24:16 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/11/17 16:22:20 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	ft_strlen(const char *str)
+{
+	int	c;
+
+	c = 0;
+	while (str[c] != '\0')
+	{
+		c ++;
+	}
+	return (c);
+}
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		j;
-	int		t;
 	char	*str;
+	int		i;
+	int		start;
+	int		end;
+	int		str_lng;
+	int		lng;
 
-	i = ft_strlen(set);
-	j = 0;
-	t = ft_strlen(s1) - (2 * i);
-	str = malloc(sizeof(char) * (ft_strlen(s1) - (2 * i)));
-	if (!str)
-		return (NULL);
-	while (j <= (t - 1))
-	{
-		str[j] = s1[j + i];
-		j ++;
-	}
+	start = 0;
+	while (set[start] == s1[start] && s1[start])
+		start ++;
+	end = 0;
+	lng = ft_strlen(s1);
+	while (set[end] == s1[lng - 1])
+		end ++;
+	str_lng = lng - end - start;
+	str = malloc(sizeof(char) * (str_lng + 1));
+	while (start < str_lng)
+		str[i ++] = s1[start ++];
 	return (str);
 }
 
-// #include <stdio.h>
-// int main()
-// {
-// 	char	*str = "\nWassup\n";
-// 	char	*set = "\n";
+#include <stdio.h>
+int main()
+{
+	char	*str = "   xxxWassup";
+	char	*set = " x";
 
-// 	printf("original :%s\nStrtrim :%s", str, ft_strtrim(str, set));
-// }
+	printf("original :%s\nStrtrim :%s", str, ft_strtrim(str, set));
+}
+
